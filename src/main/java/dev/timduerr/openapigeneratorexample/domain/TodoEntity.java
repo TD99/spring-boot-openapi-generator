23d2b9aeae70d255dcf1e8c6d3ad4a14ec968488
Tim Dürr <tim@duerrs.ch>
2025-09-06 18:36:34 +0200

    Add JPA-based persistence for Todos, update OpenAPI spec, and enhance project configuration.

---
+++ src/main/java/dev/timduerr/openapigeneratorexample/domain/TodoEntity.java
@@ -0,0 +1,50 @@
+package dev.timduerr.openapigeneratorexample.domain;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+/**
+ * TodoEntity.
+ *
+ * @author Tim Dürr
+ * @version 1.0
+ */
+@Entity
+@Table(name = "tbl_todo")
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "completed", nullable = false)
+    private boolean completed;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
---
+++ src/main/java/dev/timduerr/openapigeneratorexample/domain/TodoRepository.java
@@ -0,0 +1,16 @@
+package dev.timduerr.openapigeneratorexample.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+/**
+ * TodoRepository.
+ *
+ * @author Tim Dürr
+ * @version 1.0
+ */
+public interface TodoRepository extends JpaRepository<TodoEntity, UUID> {
+
+
+}
