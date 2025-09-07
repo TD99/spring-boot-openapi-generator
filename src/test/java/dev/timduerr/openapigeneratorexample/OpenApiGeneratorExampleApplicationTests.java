533b645b367c20085b36bd70f787c4359ed0ab25
Tim Dürr <tim@duerrs.ch>
2025-09-07 11:08:54 +0200

    Add Spring Boot test starter dependency and minimal context load test #4

---
+++ src/test/java/dev/timduerr/openapigeneratorexample/OpenApiGeneratorExampleApplicationTests.java
@@ -0,0 +1,18 @@
+package dev.timduerr.openapigeneratorexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * OpenApiGeneratorExampleApplicationTests.
+ *
+ * @author Tim Dürr
+ * @version 1.0
+ */
+@SpringBootTest
+public class OpenApiGeneratorExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
